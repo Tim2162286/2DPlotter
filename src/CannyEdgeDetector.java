9a6959943f889f285634ac3827ca7bcb329fa85c
Tim2162286 <tjcuprak@gmail.com>
2016-02-08 16:53:29 -0700

    Grayscaling fixed, started convolution method.

--- src/CannyEdgeDetector.java
+++ src/CannyEdgeDetector.java
@@ -1,12 +1,9 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URL;
+
 
 /**
  * Created by Tim on 2/8/2016.
@@ -16,9 +13,8 @@ public class CannyEdgeDetector implements EdgeDetector {
     private BufferedImage image;
 
     public boolean[][] getEdgeMatrix() throws IOException {
-        int[][] grayImage = convertToGrayscale(image);
-        matrixToImage(grayImage);
-
+        BufferedImage grayImg = convertToGrayScale(image);
+        int[][] grayArray = imageToMatrix(grayImg);
         return new boolean[0][];
     }
 
@@ -26,23 +22,21 @@ public class CannyEdgeDetector implements EdgeDetector {
         this.image = img;
 
     }
-    public int[][] convertToGrayscale(BufferedImage img) throws IOException{
-        File output = new File("original.jpg");
-        ImageIO.write(img, "jpg", output);
-        int[] dims = {img.getHeight(),img.getWidth()};
-        int[][] grayImage = new int[dims[0]][dims[1]];
-        for (int i=0; i<dims[1];i++){
-            for (int j=0; j<dims[0];j++){
-                int rgb = img.getRGB(i,j);
-                int r = (rgb)&0xFF;
-                int g = (rgb>>8)&0xFF;
-                int b = (rgb>>16)&0xFF;
-                grayImage[j][i] = (r+g+b)/3;
-            }
-        }
-        return grayImage;
+
+    public static BufferedImage convertToGrayScale(BufferedImage image) throws IOException {
+        BufferedImage result = new BufferedImage(
+                image.getWidth(),
+                image.getHeight(),
+                BufferedImage.TYPE_BYTE_GRAY);
+        Graphics g = result.getGraphics();
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+        File output = new File("GrayScaled.jpg");
+        ImageIO.write(result, "jpg", output);
+        return result;
     }
-    public BufferedImage matrixToImage(int[][] matrix) throws IOException{
+
+    public BufferedImage matrixToImage(int[][] matrix,String name) throws IOException{
         int width = matrix.length;
         int height = matrix[0].length;
         BufferedImage img = new BufferedImage(height,width,BufferedImage.TYPE_BYTE_GRAY);
@@ -52,8 +46,32 @@ public class CannyEdgeDetector implements EdgeDetector {
                 img.setRGB(j,i,gray.getRGB());
             }
         }
-        File output = new File("grayscale.jpg");
+        File output = new File(name+".jpg");
         ImageIO.write(img, "jpg", output);
         return img;
+    }
+    private int[][] imageToMatrix(BufferedImage img){
+        int width = img.getWidth();
+        int height = img.getHeight();
+        int[][] matrix = new int[height][width];
+        for (int i=0;i<width;i++){
+            for (int j=0;j<height;j++){
+                Color c = new Color(img.getRGB(i,j));
+                matrix[j][i] = c.getRed();
+            }
+        }
+        return matrix;
+    }
+    private void printMatrix(int[][] matrix){
+        for (int[] i:matrix){
+            for (int j:i)
+                System.out.print(j+" ");
+            System.out.print("\n");
+        }
+    }
+
+    private int[][] convolve(int[][] mask, int[][] img){
+        int radius = mask.length/2;
+        int width = img
     }
 }
