54d290aaf5fffa96cd9a5319ce28e1dc658438db
JonathanBush <buoywestone@gmail.com>
2016-02-09 11:28:04 -0700

    Added testing methods and edge matrix loading to ParametricSplineInterpolator.

--- src/ParametricSplineInterpolator.java
+++ src/ParametricSplineInterpolator.java
@@ -7,14 +7,19 @@ import java.util.ArrayList;
  */
 public class ParametricSplineInterpolator implements SplineGenerator {
 
+    private boolean[][] edgeMatrix;
+
     public static void main(String[] args){System.out.println("Hello World!");}
 
 	/**
      * Receives the boolean 2D array containing information about the
      * locations of edges in the image.
-     * @param edges boolean[][] true if the pixel is on an edge, false if not
+     * @param edgeMatrix boolean[][] true if the pixel is on an edge, false if not
      */
-    public void setEdgeMatrix(boolean[][] edges){}
+    public void setEdgeMatrix(boolean[][] edgeMatrix){
+        this.edgeMatrix = edgeMatrix;
+        System.out.println("Edge matrix loaded.");
+    }
 
     /**
      * Gets an ArrayList containing the coefficients for the parametric
@@ -23,6 +28,8 @@ public class ParametricSplineInterpolator implements SplineGenerator {
      * @return ArrayList containing one parametric spline for each edge
      */
     public ArrayList<ArrayList<Double[]>> getCoefficients(int resolution){
-    	return new ArrayList<ArrayList<Double[]>>();
+        int x = edgeMatrix.length/2;
+        int y = edgeMatrix[0].length/2;
+    	return new ArrayList<>();
     }
 }
