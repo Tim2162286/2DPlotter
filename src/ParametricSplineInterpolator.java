a45a40795affc7ddc358ef65ded524bb8d73f70a
JonathanBush <buoywestone@gmail.com>
2016-02-09 01:07:29 -0700

    Corrected incorrect javadoc comments.

--- src/ParametricSplineInterpolator.java
+++ src/ParametricSplineInterpolator.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 
 /**
- * Created by Tim on 2/8/2016.
+ * Creates parametric splines from edge detection output
+ * @author Jonathan Bush
+ * @since 2/9/2016
  */
 public class ParametricSplineInterpolator implements SplineGenerator {
-	
+
+    public static void main(String[] args){System.out.println("Hello World!");}
+
 	/**
      * Receives the boolean 2D array containing information about the
      * locations of edges in the image.
@@ -16,7 +20,7 @@ public class ParametricSplineInterpolator implements SplineGenerator {
      * Gets an ArrayList containing the coefficients for the parametric
      * splines that compose the image.
      * @param resolution the number of pixels to skip when generating the splines
-     * @returns ArrayList containing one parametric spline for each edge
+     * @return ArrayList containing one parametric spline for each edge
      */
     public ArrayList<ArrayList<Double[]>> getCoefficients(int resolution){
     	return new ArrayList<ArrayList<Double[]>>();
